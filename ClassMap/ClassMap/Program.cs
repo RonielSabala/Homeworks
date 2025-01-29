6d0845bcc91f20f857f48bedf9a234054aa61690
Roniel Sabala <ronielsabala1@gmail.com>
2025-01-28 22:24:33 -0400

    Creating the class map

--- ClassMap/ClassMap/Program.cs
+++ ClassMap/ClassMap/Program.cs
@@ -1,2 +1,58 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Roniel Sabala. 20240212
+
+class MiembroDeLaComunidad
+{
+
+}
+
+
+class Empleado: MiembroDeLaComunidad
+{
+
+}
+
+
+class Estudiante : MiembroDeLaComunidad
+{
+
+}
+
+
+class ExAlumno : MiembroDeLaComunidad
+{
+
+}
+
+
+class Docente: Empleado
+{
+
+}
+
+
+class Administrativo: Empleado
+{
+
+}
+
+
+class Administrador: Docente
+{
+
+}
+
+
+class Maestro: Docente
+{
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
