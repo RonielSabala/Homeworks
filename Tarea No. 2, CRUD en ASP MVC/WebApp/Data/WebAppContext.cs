e30cdfaed7a7619b6a28c4166b7a5a62d2bec2d5
Roniel Sabala <ronielsabala11@gmail.com>
2025-02-08 17:20:44 -0400

    Creation of my first CRUD for books

---
+++ Tarea No. 2, CRUD en ASP MVC/WebApp/Data/WebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Entities;
+
+namespace WebApp.Data
+{
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext (DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } = default!;
+    }
+}
---
+++ Tarea No. 2, CRUD en ASP MVC/WebApp/Models/Entities/Books.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Isbn { get; set; }
+    }
+}
